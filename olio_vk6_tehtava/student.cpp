bbd39e7c71d46b1187a319c537f151d7bbedccf9
TiitusPinola <tiitus.pinola@gmail.com>
2025-02-11 10:12:15 +0200

    Add files via upload

---
+++ olio_vk6_tehtava/student.cpp
@@ -0,0 +1,33 @@
+#include "student.h"
+#include <iostream>
+
+Student::Student(string nimi, int luku) {
+    Name = nimi;
+    Age = luku;
+}
+
+void Student::setAge(int luku)
+{
+    Age = luku;
+}
+
+void Student::setName(string nimi)
+{
+    Name = nimi;
+}
+
+string Student::getName()
+{
+    return Name;
+}
+
+int Student::getAge()
+{
+    return Age;
+}
+
+void Student::printStudentInfo()
+{
+    cout<<"Opiskelijan nimi = "<<Name<<endl;
+    cout<<"Opiskelijan ika = "<<Age<<endl;
+}
